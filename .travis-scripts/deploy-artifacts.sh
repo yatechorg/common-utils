0b1c40c0090ad2993d089487eddd2e7e8abd6232
Yinon Avraham <yinonavraham@gmail.com>
2017-07-24 00:03:15 +0300

    Deploy snapshot or release when building with jdk8
    (the only java version used currently...)

--- .travis-scripts/deploy-artifacts.sh
+++ .travis-scripts/deploy-artifacts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Inspired by http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
 
-if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_JDK_VERSION}" == "oraclejdk7" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ]; then
 
   echo "Deploying artifacts..."
   ./gradlew publishSnapshotOrRelease
